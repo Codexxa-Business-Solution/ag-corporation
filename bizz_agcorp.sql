e6636a8d46af9331fde7588ee2c6831125ac081f
payalpanjabi <payal@codexxa.in>
2022-02-14 12:39:09 +0530

    .sql file added

--- bizz_agcorp.sql
+++ bizz_agcorp.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 16, 2021 at 10:53 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.23
+-- Host: localhost
+-- Generation Time: Feb 14, 2022 at 12:35 PM
+-- Server version: 10.3.29-MariaDB-0ubuntu0.20.04.1
+-- PHP Version: 7.3.29-1+focal
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `agcorp`
+-- Database: `bizz_agcorp`
 --
 
 -- --------------------------------------------------------
@@ -79,11 +79,17 @@ CREATE TABLE `password_resets` (
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_name` varchar(225) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `dob` date NOT NULL,
-  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(225) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(225) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `state` varchar(225) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `zip` varchar(225) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user',
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -93,9 +99,15 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `dob`, `avatar`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'sopan', 'sopan@codexxa.in', '2021-06-01', '/images/1626945867.png', NULL, '$2y$10$iBqsbnIUfKPqq1TcRBOqw.p5zK3sHrCocjS7pa486Pr8XUKmwTJie', NULL, '2021-07-22 03:54:27', '2021-07-22 03:54:27'),
-(2, 'Payal', 'payal@codexxa.in', '2021-06-01', '/images/1626945867.png', NULL, '$2y$10$iBqsbnIUfKPqq1TcRBOqw.p5zK3sHrCocjS7pa486Pr8XUKmwTJie', NULL, '2021-07-22 03:54:27', '2021-07-22 03:54:27');
+INSERT INTO `users` (`id`, `name`, `customer_name`, `email`, `mobile`, `city`, `state`, `zip`, `dob`, `avatar`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'sopan', NULL, 'sopan@codexxa.in', NULL, NULL, NULL, NULL, '2021-06-01', '/images/1626945867.png', NULL, '$2y$10$iBqsbnIUfKPqq1TcRBOqw.p5zK3sHrCocjS7pa486Pr8XUKmwTJie', 'user', NULL, '2021-07-22 03:54:27', '2021-07-22 03:54:27'),
+(2, 'Payal', NULL, 'payal@codexxa.in', NULL, NULL, NULL, NULL, '2021-06-01', '/images/1626945867.png', NULL, '$2y$10$iBqsbnIUfKPqq1TcRBOqw.p5zK3sHrCocjS7pa486Pr8XUKmwTJie', 'admin', NULL, '2021-07-22 03:54:27', '2021-07-22 03:54:27'),
+(3, 'Amit yele', 'amit', 'yeleamit9@gmail.com', '1111111111111', 'vbnv', 'bnv', 'bnvb', NULL, NULL, NULL, '$2y$10$l6wgrno4qHvxiwn5VuyJ..qn5a/I4Mz.zM3TfvQ1BNTHs6/EatYlO', 'user', NULL, '2022-01-01 02:38:43', '2022-01-01 02:38:43'),
+(4, 'aman', 'Amit Tilakchand yele', 'aman@gmail.com', '08788255459', 'GOREGAON GONDIYA DISTRICT', 'Maharashtra', '441801', NULL, NULL, NULL, '$2y$10$ijKJBW6y/qC80jYgijs4AOUq4/7jgQaJLTWZaLrKOWOtNHIwpDgXe', 'user', NULL, '2022-01-02 23:30:50', '2022-01-02 23:30:50'),
+(5, 'test', 'Amit Tilakchand yele', 'test8881@yopmail.com', '08788255459', 'GOREGAON GONDIYA DISTRICT', 'Maharashtra', '441801', NULL, NULL, NULL, '$2y$10$yPUgTKvuMw97MPJyFzx5BOl4rwKV52.YEpS848Vzk6O0bkxXyzAni', 'user', NULL, '2022-01-02 23:34:40', '2022-01-02 23:34:40'),
+(6, 'amit', 'test', 'test81@yopmail.com', '08788255459', 'GOREGAON GONDIYA DISTRICT', 'Maharashtra', '441801', NULL, NULL, NULL, '$2y$10$sPApua2.jZooSa0bO9uX0.r7xhzb41751vZ2pjNsdZrndguQabdYW', 'user', NULL, '2022-01-02 23:35:34', '2022-01-02 23:35:34'),
+(7, 'manish', 'manish', 'manish@yopmail.com', '8411083218', 'GOREGAON GONDIYA DISTRICT', 'Maharashtra', '441801', NULL, NULL, NULL, '$2y$10$.X1JyshEj1Bp.mdRX514DetJKzW8zAw/Z8Jk0ykmwucSEe2us/OBG', 'user', NULL, '2022-01-03 00:11:31', '2022-01-03 00:11:31'),
+(8, 'amaama', 'amit', 'amit@codexxa.in', '08788255459', 'GOREGAON GONDIYA DISTRICT', 'Maharashtra', '441801', NULL, NULL, NULL, '$2y$10$a8/2UjabDgBbSL2bPg5Gm.jnV61BuLxgrxFkNpucyJaBCB1lfJ9sq', 'user', NULL, '2022-02-01 06:41:47', '2022-02-01 06:41:47');
 
 --
 -- Indexes for dumped tables
@@ -147,7 +159,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
